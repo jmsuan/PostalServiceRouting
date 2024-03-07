3d9d95d3c8fec7ecdef31fe872c3309b22c34781
Jonathan Lane <73461052+jmsuan@users.noreply.github.com>
2024-03-06 21:48:34 -0800

    Implement basic HashTable (PostalHashTable)

--- src/postal_hash_table.py
+++ src/postal_hash_table.py
@@ -2,24 +2,24 @@ class PostalHashTable:
     def __init__(self, size):
         self._buckets = [[] for i in range(size)]
 
-    # TODO: Support key-value pairs
-    def insert(self, key):
+    def insert(self, key, value=None):
         key_hash = key.__hash__()
         bucket_num = key_hash % len(self._buckets)
-        self._buckets[bucket_num].append(key)
+        bucket = self._buckets[bucket_num]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, value)
+                return
+        self._buckets[bucket_num].append((key, value))
 
     def lookup(self, key):
         key_hash = key.__hash__()
         bucket_num = key_hash % len(self._buckets)
         bucket = self._buckets[bucket_num]
-        if len(bucket) > 1:
-            for item in bucket:
-                if item is key:
-                    return item
-        elif len(bucket) == 0:
-            return None
-        else:
-            return bucket[0]
+        for item, value in bucket:
+            if item == key:
+                return value
+        return None
 
     def get_buckets(self):
         return self._buckets
