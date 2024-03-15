c0756e4ca4bfa8ffe8d3fde9480de87e260b57b7
Jonathan Lane <73461052+jmsuan@users.noreply.github.com>
2024-03-15 03:29:00 -0700

    Begin Scheduler

--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
 # Student ID: 001264312
-"""
-WIP NOTES:
-Ideally, the program flows as follows:
-- Import locations and their distances from each other
-- Import packages and associate with locations
-- (Calculate priority of packages?)
-- Add packages to trucks based on (package priority? use time variable to track current time?)
-- Packages should be updated by truck action methods (load, drive, deliver(maybe a Driver method?))
-"""
 
 from hash_table import HashTable
 from interface import Interface
+from location import Location
+
+# Print WIP Notes TODO: Remove WIP notes
+print("WIP NOTES:\n"
+      "Ideally, the program flows as follows:\n"
+      "- Import locations and their distances from each other\n"
+      "- Import packages and associate with locations\n"
+      "- (Calculate priority of packages?)\n"
+      "        Assign package priorities based on multiple scoring factors:\n"
+      "        distance to hub\n"
+      "        avg distance from other destinations(?)\n"
+      "        special priority\n"
+      "        deadline\n"
+      "        HOW MANY other high priority packages are nearby that facilitate ideal “elevator” routes. "
+      "(Second-pass priority?)\n"
+      "        Could calc potential facilitators based off if the dist to hub is greater than itself, and if the "
+      "distance is under some value\n"
+      "\n"
+      "- Add packages to trucks based on (package priority? use time variable to track current time?)\n"
+      "- Packages should be updated by truck action methods (load, drive, deliver(maybe a Driver method?))\n")
 
 # Import location info
 raw_locations = Interface.read_csv("../data/distance_info.csv")
-location_list = Interface.list_to_location_list(raw_locations)
+Interface.list_to_location_list(raw_locations)
 
-''' (rest of program)
-# Create custom HashTable object
+# Set the HUB for this instance
+wgu = Location.get_location_by_name("Western Governors University")
+Interface.set_hub(wgu)
+
+# Create custom HashTable object to hold packages
 pkg_table = HashTable(50)
 
 # Read CSV package data
@@ -35,4 +49,3 @@ for pkg in pkg_list:
 # Print all packages in HashTable in a table format
 print("Package Table:")
 Interface.print_package_table(pkg_table.values(pkg_ids))
-'''
---
+++ src/scheduler.py
@@ -0,0 +1,20 @@
+from package import Package
+
+class Scheduler:
+    @staticmethod
+    def prioritize_packages(package_list: list[Package]) -> list[int]:
+        """
+        Assign package priorities based on multiple scoring factors:
+            - distance to hub(?)
+            - avg distance from other destinations(?)
+            - special priority!
+            - deadline!
+            - HOW MANY other high priority packages are nearby that facilitate ideal “elevator” routes.
+                (Second-pass priority?)
+            - Could calc potential facilitators based off if the dist to hub is greater than itself, and if the
+                distance is under some value
+
+        :param package_list:
+        :return:
+        """
+        pass  # TODO: Implement
