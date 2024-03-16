4f64f26e5b657902232f04e49a189553e66b1f4a
Jonathan Lane <73461052+jmsuan@users.noreply.github.com>
2024-03-16 02:53:24 -0700

    Explained the plan for generating routes.

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,7 +1,36 @@
 from package import Package
+from location import Location
 
 
 class Scheduler:
+    """
+    The scheduler is divided into two parts to simplify mileage optimization.
+
+    First, the Scheduler generates routes from the hub, using the list of Locations.
+    - The routes aim to achieve optimal milage by creating static routes that hit the most Locations with the smallest
+      total distance travelled. This means that a single route ideally follow a straight line out from the hub to the
+      furthest destination, then comes back (stopping to deliver packages to locations along the line).
+    - However, in practice, the Locations that need to be delivered to are never all located in a straight line. So this
+      routing algorithm aims to generate routes that look like "petals" or "ellipses." These petals should have as many
+      routes on them as possible, while aiming to minimize the "deviance" it has from a theoretical straight line from
+      the hub to the furthest Location on the route. We can measure this deviance by taking the total distance of the
+      route and comparing it to a round trip from the hub to the furthest location.
+    - This solution takes into consideration that the trucks have a limited capacity, so they will need to return to the
+      hub anyway. This gives the truck the opportunity to follow another "semi-ideal" route that starts from the hub.
+      The main benefit to this approach is the fact that it's easy to calculate the routes from a list of Locations,
+      when only the distances between the Locations are given.
+    - In the case that a Truck needs to reuse a route because it could not carry all the packages for locations on the
+      route the first time, it will only travel to the Locations that need to be delivered to (in the order that is
+      specified by the route).
+
+    Next, the Scheduler generates "priorities" for each package. This takes into account the following characteristics:
+    - How far it is from the HUB. (This is to prioritize delivering the furthest packages first within a route, which is
+      beneficial to the total mileage if the Truck cannot deliver to all the locations on a route at once.)
+    """
+    @staticmethod
+    def generate_routes(hub: Location, location_list: list[Location], ):
+        pass
+
     @staticmethod
     def prioritize_packages(package_list: list[Package]) -> list[int]:
         """
