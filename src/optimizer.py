8357d42cf55ace25533dd6638e3830142c6fa932
Jonathan Lane <73461052+jmsuan@users.noreply.github.com>
2024-03-19 13:54:31 -0700

    Updated and clarified Optimizer DocString.

--- src/optimizer.py
+++ src/optimizer.py
@@ -7,18 +7,18 @@ from route_set import RouteSet
 
 class Optimizer:
     """
-    The Optimizer is divided into two parts to simplify mileage optimization.
+    The Optimizer is divided into two parts to help simplify mileage optimization.
 
     First, the Optimizer generates routes from the hub, using the list of Locations.
 
     - The routes aim to achieve optimal milage by creating static routes that hit the most Locations with the smallest
-      total distance travelled. This means that a single route ideally follow a straight line out from the hub to the
+      total distance travelled. This means that a single route ideally follows a straight line out from the hub to the
       furthest destination, then comes back (stopping to deliver packages to locations along the line).
     - However, in practice, the Locations that need to be delivered to are never all located in a straight line. So this
       routing algorithm aims to generate routes that look like "petals" or "ellipses." These petals should have as many
-      routes on them as possible, while aiming to minimize the "deviance" it has from a theoretical straight line from
-      the hub to the furthest Location on the route. We can measure this deviance by taking the total distance of the
-      route and comparing it to a round trip from the hub to the furthest location.
+      Locations on them as possible, while aiming to minimize the "deviance" it has from the theoretical straight line
+      from the hub to the furthest Location on the route. We can measure this deviance by taking the total distance of
+      the route and comparing it to a round trip from the hub to the furthest location.
     - This solution takes into consideration that the trucks have a limited capacity, so they will need to return to the
       hub anyway. This gives the truck the opportunity to follow another "semi-ideal" route that starts from the hub.
       The main benefit to this approach is the fact that it's easy to calculate the routes from a list of Locations,
@@ -31,14 +31,14 @@ class Optimizer:
 
     - The deadline of the package.
     - Any special truck requirements (e.g. a package that needs to be delivered by a truck with a certain id).
-    - How distance from the HUB. (This is to prioritize delivering the furthest packages first within a route, which is
-      beneficial to the total mileage if the Truck cannot deliver to all the locations on a route at once.)
-    - How many other high priority packages are nearby that facilitate ideal "elevator" routes. (This is a second-pass
-      priority, as it is not as important as the other factors. It is also more difficult to calculate, as it requires
-      the routes to be generated first.)
-    - The Optimizer will then assign the packages to the Trucks in the order of their priority. The Trucks will then
-      follow the routes that were generated earlier, and deliver the packages in the order that they were assigned, but
-      only if the package is on the route that the Truck is following.
+    - How distant it is  from the HUB. (This is to prioritize delivering the furthest packages first within a route,
+      which is beneficial to the total mileage if the Truck cannot deliver to all the locations on a route at once.)
+    - How many other high priority packages are nearby. (This is a second-pass priority, as it requires the other
+      characteristics to be calculated first.)
+
+    The Optimizer can then assign the packages to the Trucks in the order of their priority. The Trucks will then follow
+    the routes that were generated earlier, and deliver the packages in the order that they were assigned, but only if
+    the package is on the route that the Truck is following.
     """
     @staticmethod
     def generate_routes(
